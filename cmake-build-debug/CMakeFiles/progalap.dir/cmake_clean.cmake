863e2a9c84bb21077c29cddfe27d5df4db5cf7c7
HajagosNorbert <hajagosnorbi@gmail.com>
2021-05-04 20:52:28 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/progalap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/progalap.dir/main.c.obj"
+  "libprogalap.dll.a"
+  "progalap.exe"
+  "progalap.exe.manifest"
+  "progalap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/progalap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
