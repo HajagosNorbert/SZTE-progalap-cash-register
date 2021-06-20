04ede1477d1543018a88d4a8d6f844485e492d34
HajagosNorbert <hajagosnorbi@gmail.com>
2021-06-20 10:39:56 +0200

    argumentum kérés

--- main.c
+++ main.c
@@ -5,15 +5,19 @@
 #include "item.h"
 #include "kiir.h"
 
-int main( int argc, char *argv[] ) {
+int main(int argc, char *argv[]) {
     //Norbert
+    char *nyugtaHossz;
+    if (argc == 2) {
+        nyugtaHossz = argv[1];
+    } else {
+        fprintf(stdout, "Adjon meg egy argumentumot, ami a nyugta hosszat jelzi! ");
+        nyugtaHossz = "20";
+    }
+
     itemNode *firstItem;
     firstItem = beolvaso();
-    if(argc==2){
-        printOutList(firstItem,argv[1]);
-    }else{
-        printOutList(firstItem,"1");
-    }
+    printOutList(firstItem, nyugtaHossz);
     freeItems(firstItem);
     return 0;
 }
